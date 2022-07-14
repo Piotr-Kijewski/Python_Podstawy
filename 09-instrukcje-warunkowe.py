7b4689ddff24581935b34bddc78f4d529f35850e
Piotr-Kijewski <piotr.kijewski@gmail.com>
2022-07-14 12:34:31 +0200

    new file

---
+++ 09-instrukcje-warunkowe.py
@@ -0,0 +1,13 @@
+light = input('Jakie jest światło? (red, yellow, green)')
+
+# if light == 'red':
+# # if str(light).startswith('r'):
+#     print('Czekaj!')
+# elif light == 'yellow':
+#     print('Przygotuj się!')
+# elif light == 'green':
+#     print('Jedź!')
+# else:
+#     print('Niewłaściwa wartość!')
+
+# print('Jedź!') if light == 'green' else print('Czekaj!')
